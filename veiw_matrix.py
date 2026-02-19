34b1ae250ae749f5d0bba6600f9b05eb01173163
solpsoldo-svg <solpsoldo@gmail.com>
2026-02-19 17:53:09 +0300

    Add files via upload

--- veiw_matrix.py
+++ veiw_matrix.py
@@ -1,26 +1,18 @@
 import numpy as np
 
 def veiw(cam_target, cam_pos, up): # cam_target, cam_pos, vector_up
-    cam_z=np.array(cam_target)-np.array(cam_pos)
-    cam_z=cam_z/np.linalg.norm(cam_z)
+    cam_z = np.array(cam_target) - np.array(cam_pos)
+    cam_z = cam_z/np.linalg.norm(cam_z)
 
-    cam_x=np.cross(up, cam_z)
-    cam_x=cam_x/np.linalg.norm(cam_x)
+    cam_x = np.cross(up, cam_z)
+    cam_x = cam_x/np.linalg.norm(cam_x)
 
-    cam_y=np.cross(cam_z, cam_x)
-    cam_y=cam_y/np.linalg.norm(cam_y)
+    cam_y = np.cross(cam_z, cam_x)
+    cam_y = cam_y/np.linalg.norm(cam_y)
 
-    rotate=np.array([
-        [cam_x[0], cam_x[1], cam_x[2], 0],
-        [cam_y[0], cam_y[1], cam_y[2], 0],
-        [cam_z[0], cam_z[1], cam_z[2], 0],
+    return np.array([
+        [cam_x[0], cam_x[1], cam_x[2], -np.dot(cam_x, cam_pos)],
+        [cam_y[0], cam_y[1], cam_y[2], -np.dot(cam_y, cam_pos)],
+        [cam_z[0], cam_z[1], cam_z[2], -np.dot(cam_z, cam_pos)],
         [0, 0, 0, 1]
     ])
-    tr=np.array([
-        [1, 0, 0, -cam_pos[0]],
-        [0, 1, 0, -cam_pos[1]],
-        [0, 0, 1, -cam_pos[2]],
-        [0, 0, 0, 1]
-    ])
-
-    return rotate @ tr
